cc5b04c1d79584788cadc0b7117f91c5a3ba909e
656yash <yash656deshpande@gmail.com>
2025-10-08 11:57:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,14 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sqlite3
 import os
-from datetime import datetime
+from datetime import datetime 
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render assigns PORT, fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
